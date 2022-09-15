4a0d2efcc6010bf1f41124356f5e8769d5402a24
Dicky Umardhani <ddickyumardhani@gmail.com>
2022-09-16 00:59:42 +0800

    update hero

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,18 @@
+import React from "react";
+import './HeroStyles.css'
+
+const Hero = () => {
+  return(
+    <div className="hero">
+      <div className="container">
+        <div className="content">
+          <h1>Cryptocurrency</h1>
+          <h1 className="blue">Tracker</h1>
+          <div><button>Learn More</button></div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
