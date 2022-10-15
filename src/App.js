4e0bd107b5be4e7a20eadd32cbd2faf6dea52a5e
tugaspstiunud <72388016+GaluhGita20@users.noreply.github.com>
2022-10-15 21:40:28 +0800

    backend push

--- src/App.js
+++ src/App.js
@@ -5,17 +5,54 @@ import AboutUs from './components/aboutUs/aboutUs';
 import News from './components/news/news';
 import Contact from './components/contact/contact';
 import Footer from './components/footer/footer';
-import Table_Realtime from "./components/chart/TableCrypto";
+import CoinMarkets from "./components/table/CoinMarkets";
+import Container from '@mui/material/Container';
+import BarChart from './components/charts/BarChart';
+import LineChart from './components/charts/LineChart';
+import Grid from '@mui/material/Grid';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+
+
+
 function App() {
   return (
     <div className='app'>
       <Navbar/>
       <Hero/>
-      <Table_Realtime/>
-      <News/>
-      <AboutUs/>
-      
-      <Contact/>
+      <Box backgroundColor="#fff">
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <CoinMarkets />
+            </Grid>
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <BarChart/>      
+            </Grid>
+          </Grid>
+          
+          <Grid item md={8} xs={12}>
+              <LineChart />
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <News/>
+            </Grid>
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <AboutUs/> 
+            </Grid>
+          </Grid>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Contact/> 
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
       <Footer/>
     </div>
   );
